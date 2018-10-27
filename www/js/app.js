23d9b76bbcc578076e0d0a08fc6c188e070e777e
danish1023 <danish1023@gmail.com>
2018-10-28 02:53:48 +0530

    added native dialer

--- www/js/app.js
+++ www/js/app.js
@@ -63,3 +63,9 @@ function showScanner() {
     }
   );
 }
+function callMe() {
+  window.plugins.CallNumber.callNumber(onSuccess, onError, "+919267965329", true);
+  function onError(result) {
+    window.plugins.toast.show("Error: "+result, 'long', 'bottom');
+  }
+}
